d3818283010e0a76e4a10ecb0ac7659759e8552b
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2022-11-30 10:31:42 -0500

    Add base aws helper scripts.

---
+++ HOME/Scripts/invoke-lambda.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws lambda invoke --function-name pascal-geocore-lambda-node_16 out --output text
---
+++ HOME/Scripts/lambda-logs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws logs tail "/aws/lambda/pascal-geocore-lambda-node_16" --follow
